2eb7c352d5a6d0332eb670c1a7b7112bc73bd8cf
Nuno Maduro <enunomaduro@gmail.com>
2025-02-15 19:40:16 +0000

    Update ClickLinkTest.php

--- tests/Browser/Operations/ClickLinkTest.php
+++ tests/Browser/Operations/ClickLinkTest.php
@@ -2,20 +2,8 @@
 
 declare(strict_types=1);
 
-use Pest\Browser\Compiler;
-
-use function Pest\Browser\visit;
-
-it('clicks a link using its text', function (): void {
-    visit('https://laravel.com')
-        ->clickLink('Get Started');
-
-    expect(file_get_contents(Compiler::TEST_PATH))
-        ->toContain("await page.locator('a').filter({ hasText: /Get Started/i }).click();");
-});
-
 it('clicks a link using its text and a given selector', function (): void {
-    visit('https://laravel.com')
+    $this->visit('https://laravel.com')
         ->clickLink('Get Started', 'a.border-red-500');
 
     expect(file_get_contents(Compiler::TEST_PATH))
