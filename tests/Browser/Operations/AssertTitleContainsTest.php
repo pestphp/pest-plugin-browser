7e3d528a7ef52c2d0e49639cc0255669bf46586a
ju5t <m@ju5t.nl>
2025-02-15 21:19:00 +0100

    chore: inline test

--- tests/Browser/Operations/AssertTitleContainsTest.php
+++ tests/Browser/Operations/AssertTitleContainsTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 test('assert title contains', function () {
     $url = 'https://laravel.com';
 
-    $browser = $this->visit($url);
-
-    $browser->assertTitleContains('Laravel');
+    $this->visit($url)
+        ->assertTitleContains('Laravel');
 });
