59883438393b368de3339b56d9ca2e0d8d94c54f
Nuno Maduro <enunomaduro@gmail.com>
2025-02-15 20:33:24 +0000

    refactor: adds missing php annotations

--- src/Operations/AssertTitleContains.php
+++ src/Operations/AssertTitleContains.php
@@ -8,12 +8,18 @@ use Pest\Browser\Contracts\Operation;
 
 final readonly class AssertTitleContains implements Operation
 {
+    /**
+     * Creates an operation instance.
+     */
     public function __construct(
         private string $title,
     ) {
         //
     }
 
+    /**
+     * Compile the operation.
+     */
     public function compile(): string
     {
         return sprintf('await expect(await page.title()).toMatch(/%s/);', $this->title);
