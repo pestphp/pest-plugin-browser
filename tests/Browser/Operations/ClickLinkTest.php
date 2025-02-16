4491b3f12436ed964ea834052568e7280e5bbd43
Clem Blanco <clement.blanco@gmail.com>
2025-02-16 18:21:10 +0100

    WIP

--- tests/Browser/Operations/ClickLinkTest.php
+++ tests/Browser/Operations/ClickLinkTest.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 it('clicks a link', function (): void {
-    $this->visit('https://laravel.com')
-        ->clickLink('Get Started')
+    $this->visit('/')
+        ->clickLink('Laravel Docs')
         ->assertUrlIs('https://laravel.com/docs/11.x');
 });
