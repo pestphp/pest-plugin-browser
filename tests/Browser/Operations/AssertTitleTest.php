9733b8e5b7647fbca5454538ea072cd4b4c75345
Jack <jjbayliss@icloud.com>
2025-02-16 02:22:22 +0000

    fake test to see cache

--- tests/Browser/Operations/AssertTitleTest.php
+++ tests/Browser/Operations/AssertTitleTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-test('assert title', function (): void {
+test('assert a title', function (): void {
     $url = 'https://laravel.com';
 
     $browser = $this->visit($url);
