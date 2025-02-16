e227cb23909acdaf8dc29ea13511c680b8dc4105
Clem Blanco <clement.blanco@gmail.com>
2025-02-16 20:06:51 +0100

    WIP

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
+import {defineConfig, devices} from '@playwright/test';
 
 /**
  * Read environment variables from file.
@@ -41,7 +41,7 @@ export default defineConfig({
     projects: [
         {
             name: 'chromium',
-            use: { ...devices['Desktop Chrome'] },
+            use: {...devices['Desktop Chrome']},
         },
 
         /*
