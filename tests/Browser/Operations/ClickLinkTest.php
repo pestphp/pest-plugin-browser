2879878ae15bc15f1697d7fc09bdd55efcdb6231
Clem Blanco <clement.blanco@gmail.com>
2025-02-17 10:53:55 +0100

    chore: fix clickLink test

--- tests/Browser/Operations/ClickLinkTest.php
+++ tests/Browser/Operations/ClickLinkTest.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 it('clicks a link', function (): void {
     $this->visit('/')
-        ->clickLink('Link')
+        ->clickLink('Get Started')
         ->assertUrlIs('https://pestphp.com/docs/installation');
 });
