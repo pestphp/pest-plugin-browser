bb53660bfc7d3df36c2c4a7c353c8e166d2e22a4
Clem Blanco <clement.blanco@gmail.com>
2025-02-17 12:54:11 +0100

    WIP

--- tests/Browser/Operations/ClickAtPointTest.php
+++ tests/Browser/Operations/ClickAtPointTest.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 it('clicks an element using x and y coordinates', function (): void {
     $this->visit('/')
-        ->assertSee('Click me')
-        ->assertDontSee('Single clicked!')
-        ->clickAtPoint(640, 970)
-        ->assertSee('Single clicked!')
-        ->assertDontSee('Click me');
+        ->clickAtPoint(530, 520)
+        ->assertUrlIs('https://pestphp.com/docs/installation');
 });
