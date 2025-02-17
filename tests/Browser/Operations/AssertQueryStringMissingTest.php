1350498afc71030e1676e8839023f25b430cb824
Muhannad Jaafari <muhanad@vega4it.com>
2025-02-17 22:57:46 +0400

    chore: update assertQueryStringMissing test description

--- tests/Browser/Operations/AssertQueryStringMissingTest.php
+++ tests/Browser/Operations/AssertQueryStringMissingTest.php
@@ -2,13 +2,13 @@
 
 declare(strict_types=1);
 
-test('assert query string does not have the givin query param with value', function () {
+test('assert query string does not have the given query param with value', function () {
     $url = 'https://laravel.com?q=test';
     $this->visit($url)
         ->assertQueryStringMissing('q', 'test-1');
 });
 
-test('assert query string has a giving only the query param', function () {
+test('assert query string does not have the only given the query param', function () {
     $url = 'https://laravel.com?q=test';
     $this->visit($url)
         ->assertQueryStringMissing('s');
