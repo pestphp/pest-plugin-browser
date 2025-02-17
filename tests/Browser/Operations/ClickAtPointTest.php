ac0923560a7b85fc23b1117df2272d44a8948d77
Clem Blanco <clement.blanco@gmail.com>
2025-02-17 13:21:04 +0100

    WIP

--- tests/Browser/Operations/ClickAtPointTest.php
+++ tests/Browser/Operations/ClickAtPointTest.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 it('clicks an element using x and y coordinates', function (): void {
     $this->visit('/')
         ->screenshot('click-at-point.png')
-        ->clickAtPoint(530, 520)
+        ->clickAtPoint(530, 550)
         ->assertUrlIs('https://pestphp.com/docs/installation');
 })->only();
--- tests/Pest.php
+++ tests/Pest.php
@@ -4,19 +4,19 @@ declare(strict_types=1);
 
 use Pest\TestSuite;
 
-pest()
-    ->beforeEach(fn () => cleanupScreenshots())
-    ->afterEach(fn () => cleanupScreenshots());
+// pest()
+//     ->beforeEach(fn () => cleanupScreenshots())
+//     ->afterEach(fn () => cleanupScreenshots());
 
-function cleanupScreenshots(): void
-{
-    $basePath = TestSuite::getInstance()->testPath.'/Browser/screenshots';
+// function cleanupScreenshots(): void
+// {
+//     $basePath = TestSuite::getInstance()->testPath.'/Browser/screenshots';
 
-    foreach (glob("$basePath/*") as $file) {
-        if (is_file($file)) {
-            unlink($file);
-        }
-    }
+//     foreach (glob("$basePath/*") as $file) {
+//         if (is_file($file)) {
+//             unlink($file);
+//         }
+//     }
 
-    file_exists($basePath) && rmdir($basePath);
-}
+//     file_exists($basePath) && rmdir($basePath);
+// }
