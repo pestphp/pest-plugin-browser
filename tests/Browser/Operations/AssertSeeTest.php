081b0487154ac221fbc9e3f0df9d84e239f09ab0
Clem Blanco <clement.blanco@gmail.com>
2025-02-17 15:40:00 +0100

    WIP

--- tests/Browser/Operations/AssertSeeTest.php
+++ tests/Browser/Operations/AssertSeeTest.php
@@ -16,8 +16,3 @@ test('assert sees escaping regex special characters', function () {
     $this->visit('https://laravel.com')
         ->assertSee('I tried (many) different ecosystems');
 });
-
-test('lol', function () {
-    $this->visit('https://laravel.com')
-        ->assertSee('“Laravel’s best-in-class testing tools');
-})->only();
