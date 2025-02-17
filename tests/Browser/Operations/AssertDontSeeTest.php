a2b47f5ed89f9193ef6fbe3086a52fb131afdb80
Clem Blanco <clement.blanco@gmail.com>
2025-02-17 15:36:19 +0100

    WIP

--- tests/Browser/Operations/AssertDontSeeTest.php
+++ tests/Browser/Operations/AssertDontSeeTest.php
@@ -5,21 +5,21 @@ declare(strict_types=1);
 test('assert does not see', function () {
     $this->visit('https://laravel.com')
         ->assertDontSee('The PHP Foobar');
-})->only();
+});
 
 test('assert does not see ignoring case by default', function () {
     expect(function () {
         $this->visit('https://laravel.com')
             ->assertDontSee('the php framework');
     })->toThrow(Exception::class);
-})->only();
+});
 
 test('assert does not see can be case sensitive', function () {
     $this->visit('https://laravel.com')
         ->assertDontSee('the php framework', false);
-})->only();
+});
 
 test('assert does not see escaping regex special characters', function () {
     $this->visit('https://laravel.com')
         ->assertDontSee('I tried (some) different ecosystems');
-})->only();
+});
--- tests/Browser/Operations/AssertSeeTest.php
+++ tests/Browser/Operations/AssertSeeTest.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 test('assert sees', function () {
     $this->visit('https://laravel.com')
         ->assertSee('The PHP Framework');
-})->only();
+});
 
 test('assert sees ignoring case', function () {
     $this->visit('https://laravel.com')
         ->assertSee('the php framework', true);
-})->only();
+});
 
 test('assert sees escaping regex special characters', function () {
     $this->visit('https://laravel.com')
         ->assertSee('I tried (many) different ecosystems');
-})->only();
+});
