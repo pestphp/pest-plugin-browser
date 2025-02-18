7fa0bd779a4d1d3d20c55994989754226f5bef51
Patricio <ijpatricio@gmail.com>
2025-02-18 18:43:22 +0000

    Separate tests for explicittly

--- tests/Browser/Operations/ForwardTest.php
+++ tests/Browser/Operations/ForwardTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-test('goes back and forward', function (): void {
+test('navigates back', function (): void {
     $this->visit('/')
         ->clickLink('Get Started')
         ->assertUrlIs('https://pestphp.com/docs/installation')
