417b2e2ac972e7614eebbe6165747925111a6613
Patricio <ijpatricio@gmail.com>
2025-02-18 20:46:57 +0000

    Add playground URL helper

--- tests/Browser/Operations/BackTest.php
+++ tests/Browser/Operations/BackTest.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-test('navigates forward', function (): void {
+test('navigates back', function (): void {
     $this->visit('/')
         ->clickLink('Get Started')
         ->assertUrlIs('https://pestphp.com/docs/installation')
         ->back()
-        ->assertUrlIs('http://localhost:9357');
+        ->assertUrlIs(playgroundUrl());
 });
--- tests/Browser/Operations/PauseTest.php
+++ tests/Browser/Operations/PauseTest.php
@@ -3,17 +3,17 @@
 declare(strict_types=1);
 
 it('pause', function (): void {
-    $this->visit('https://laravel.com')
-        ->clickLink('Get Started')
-        ->pause(5000)
-        ->assertUrlIs('https://laravel.com/docs/11.x');
+    $this->visit(playgroundUrl('/test/interactive-elements'))
+        ->assertDontSee('I appear after 2 seconds')
+        ->pause(2200)
+        ->assertSee('I appear after 2 seconds');
 });
 
 test('throws an exception when pause is negative', function (): void {
     expect(function () {
         $this->visit('https://laravel.com')
             ->clickLink('Get Started')
-            ->pause(-5000)
+            ->pause(-1000)
             ->assertUrlIs('https://laravel.com/docs/11.x');
     })
         ->toThrow(
