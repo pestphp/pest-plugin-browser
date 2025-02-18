590d7d76e30cc3de9bbcdc846c86d11f0345c49c
Patricio <ijpatricio@gmail.com>
2025-02-18 21:29:20 +0000

    Fix: Pint

--- playground/app/View/Components/TestPagesNavMenu.php
+++ playground/app/View/Components/TestPagesNavMenu.php
@@ -1,23 +1,27 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\View\Components;
 
 use Closure;
+use File;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 use Illuminate\View\Component;
 use Symfony\Component\Finder\SplFileInfo;
 
-class TestPagesNavMenu extends Component
+final class TestPagesNavMenu extends Component
 {
     public Collection $pages;
+
     /**
      * Create a new component instance.
      */
     public function __construct()
     {
-        $this->pages = collect(\File::allFiles(resource_path('views/test-pages')))
+        $this->pages = collect(File::allFiles(resource_path('views/test-pages')))
             ->map(fn (SplFileInfo $viewFile) => $viewFile->getFilename())
             ->map(fn (string $filename) => Str::chopEnd($filename, '.blade.php'));
     }
