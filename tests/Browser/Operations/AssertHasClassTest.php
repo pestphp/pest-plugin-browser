9670a12ef8b4bcf490d7b966a12af8c99d557b43
Alisson de Padua <a.depadua@cliqdigital.com>
2025-02-19 00:38:44 +0100

    feat: has class

---
+++ tests/Browser/Operations/AssertHasClassTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+describe('assert has class', function () {
+    it('asserts that the element has a single class', function () {
+        $this->visit('/')
+            ->assertHasClass('#div-1', 'class-1');
+    });
+
+    it('asserts that the element has multiple classes', function () {
+        $this->visit('/')
+            ->assertHasClass('#div-3', 'class-1 selected class-3');
+    });
+
+    it('asserts that the element has a regex-based class match', function () {
+        $this->visit('/')
+            ->assertHasClass('#div-3', '/(^|\s)selected(\s|$)/');
+    });
+
+    it('asserts that the list of elements located matches the corresponding list of expected class', function () {
+        $this->visit('/')
+            ->assertHasClass('ul > .component', ['component', 'component selected', 'component']);
+    });
+});
