7f6fa21e4bf4c8e133d0484987d1ec2a34560aa3
Jérôme Leontidis <jerome.leontidis@mathspartner.ch>
2025-02-19 15:53:08 +0100

    feat:added new operations - Check, UnCheck & AssertChecked, AssertNotChecked

--- tests/Browser/Operations/AssertNotCheckedTest.php
+++ tests/Browser/Operations/AssertNotCheckedTest.php
@@ -3,8 +3,6 @@
 declare(strict_types=1);
 
 test('assert checkbox is not checked', function () {
-    $url = 'http://localhost:9357';
-
-    $this->visit($url)
-        ->assertNotChecked('input[name="checkbox-unchecked"]');
+    $this->visit('/test/form-inputs')
+        ->assertNotChecked('input[name="default-checkbox"]');
 });
--- tests/Browser/Operations/UnCheckTest.php
+++ tests/Browser/Operations/UnCheckTest.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 test('uncheck the checked checkbox', function () {
-    $this->visit('http://localhost:9357')
-        ->uncheck('input[name="checkbox-checked"]')
-        ->assertNotChecked('input[name="checkbox-checked"]');
+    $this->visit('/test/form-inputs')
+        ->uncheck('input[name="checked-checkbox"]')
+        ->assertNotChecked('input[name="checked-checkbox"]');
 });
