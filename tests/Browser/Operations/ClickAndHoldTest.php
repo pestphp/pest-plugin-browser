ba116bf5d0ed8305d6d7fde3518d34d7becb9e64
Clem Blanco <clement.blanco@gmail.com>
2025-02-20 00:15:15 +0100

    fix: flaky click tests

--- tests/Browser/Operations/ClickAndHoldTest.php
+++ tests/Browser/Operations/ClickAndHoldTest.php
@@ -24,12 +24,12 @@ it('can click and hold multiple times', function (): void {
         ->clickAndHold('button[data-testId="hold-click"]')
         ->clickAndHold('button[data-testId="hold-click"]')
         ->assertSee('Free hug! \(3\)');
-});
+})->only()->repeat(50);
 
 it('can click and hold for a given duration', function (): void {
     $this->visit('/test/interacting-with-elements')
-        ->clickAndHold('button[data-testId="hold-click"]', 300)
+        ->clickAndHold('button[data-testId="hold-click"]', 400)
         ->assertDontSee('Free hug!')
         ->clickAndHold('button[data-testId="hold-click"]', 1500)
         ->assertSee('Free hug!');
-});
+})->only()->repeat(50);
