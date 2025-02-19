99ea169afb32df204d225916db4ae9ae5b3935b1
Clem Blanco <clement.blanco@gmail.com>
2025-02-20 00:42:00 +0100

    fix(ci): click at point flakiness

--- tests/Browser/Operations/ClickAtPointTest.php
+++ tests/Browser/Operations/ClickAtPointTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 it('clicks an element using x and y coordinates', function (): void {
     $this->visit('/test/interacting-with-elements')
+        ->assertSee('Home')
         ->clickAtPoint(320, 640)
         ->assertUrlIs(playgroundUrl());
 });
