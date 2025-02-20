dbb9b8ef3d94c4cf5f75b9cf6228d4a86855afef
Clem Blanco <clement.blanco@gmail.com>
2025-02-20 11:07:45 +0100

    adjust assertSee uage in tests accordingly

--- tests/Browser/Operations/DoubleClickTest.php
+++ tests/Browser/Operations/DoubleClickTest.php
@@ -5,23 +5,23 @@ declare(strict_types=1);
 it('double clicks an element using css selectors', function (): void {
     $this->visit('/test/interacting-with-elements')
         ->assertSee('Double click me')
-        ->assertDontSee('Double clicked\!')
+        ->assertDontSee('Double clicked!')
         ->doubleClick("button[data-testId='double-click']")
-        ->assertSee('Double clicked\!')
+        ->assertSee('Double clicked!')
         ->assertDontSee('Double click me');
 });
 
 it('escapes double quotes properly', function () {
     $this->visit('/test/interacting-with-elements')
         ->doubleClick('button[data-testId="double-click"]')
-        ->assertSee('Double clicked\!');
+        ->assertSee('Double clicked!');
 });
 
 it('can double click multiple times', function (): void {
     $this->visit('/test/interacting-with-elements')
         ->doubleClick('button[data-testId="double-click"]')
-        ->assertSee('Double clicked\! \(1\)')
+        ->assertSee('Double clicked! (1)')
         ->doubleClick('button[data-testId="double-click"]')
         ->doubleClick('button[data-testId="double-click"]')
-        ->assertSee('Double clicked\! \(3\)');
+        ->assertSee('Double clicked! (3)');
 });
