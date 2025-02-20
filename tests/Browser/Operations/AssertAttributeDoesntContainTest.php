9c568890c10a13f647e17551c9725cd67da7316b
Jack <jjbayliss@icloud.com>
2025-02-20 01:16:24 +0000

    Update AssertAttributeDoesntContainTest.php

--- tests/Browser/Operations/AssertAttributeDoesntContainTest.php
+++ tests/Browser/Operations/AssertAttributeDoesntContainTest.php
@@ -4,5 +4,5 @@ declare(strict_types=1);
 
 test('assert attribute doesnt contain', function () {
     $this->visit('/test/interactive-elements')
-        ->assertAttributeContains('#i-have-data-testid', 'data-testid', 'not-to-be-seen');
+        ->assertAttributeDoesntContain('#i-have-data-testid', 'data-testid', 'not-included');
 });
