80bf4736323882f8ac49040665e4473444481ce7
Clem Blanco <clement.blanco@gmail.com>
2025-02-20 18:11:22 +0100

    WIP

--- tests/Browser/Operations/ClickAtPointTest.php
+++ tests/Browser/Operations/ClickAtPointTest.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 it('clicks an element using x and y coordinates', function (): void {
     $this->visit('/test/interacting-with-elements')
-        ->assertSee('Home')
-        ->pause(500)
-        ->clickAtPoint(320, 640)
-        ->assertUrlIs(playgroundUrl());
+        ->clickAtPoint(1050, 70)
+        ->assertSee('Single clicked!');
 });
