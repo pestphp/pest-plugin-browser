48d111bacad086270a7c93b9478d5314294fb1be
Clem Blanco <clement.blanco@gmail.com>
2025-02-20 13:41:31 +0100

    WIP

--- tests/Browser/Operations/AssertAttributeContainsTest.php
+++ tests/Browser/Operations/AssertAttributeContainsTest.php
@@ -4,5 +4,12 @@ declare(strict_types=1);
 
 test('assert attribute contains', function () {
     $this->visit('/test/interactive-elements')
-        ->assertAttributeContains('#i-have-data-testid', 'data-testid', 'to-be-seen');
-});
+        ->assertSee('Foo');
+    // ->assertAttributeContains('#i-have-data-testid', 'data-testid', 'to-be-seen');
+})->only();
+
+test('assert bar', function () {
+    $this->visit('/test/interactive-elements')
+        ->assertSee('Bar');
+    // ->assertAttributeContains('#i-have-data-testid', 'data-testid', 'to-be-seen');
+})->only();
